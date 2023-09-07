b06993c9763aa698d14b0fcb8b78a1ba4b58037c
omerc <omerc@DESKTOP-P7FDVCL>
2023-09-07 14:15:11 +0300

    .

--- StokTakip.WebApp/Controllers/CategoryController.cs
+++ StokTakip.WebApp/Controllers/CategoryController.cs
@@ -1,26 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StokTakip.WebApp.Repository;
 
-namespace StokTakip.WebApp.Controllers
+namespace StokTakip.WebApp.Controllers;
+
+public class CategoryController : Controller
 {
-    public class CategoryController : Controller
+
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryController(ICategoryRepository categoryRepository)
     {
-
-        private readonly ICategoryRepository _categoryRepository;
-
-        public CategoryController(ICategoryRepository categoryRepository)
-        {
-            _categoryRepository = categoryRepository;
-        }
-
-
-
-
-
-        public IActionResult Index()
-        {
-            var categories = _categoryRepository.GetAll();
-            return View(categories);
-        }
+        _categoryRepository = categoryRepository;
     }
+
+
+    public IActionResult Index()
+    {
+        var categories = _categoryRepository.GetAll();
+        return View(categories);
+    }
+
+
+
+
+
+
+
 }
